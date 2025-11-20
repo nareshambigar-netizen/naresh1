0d14c5bc5951f49f811cffad521355ba98e66709
naresh1 <nareshambigar@gmail.com>
2025-11-19 22:34:49 -0800

    java

--- naresh1.css
+++ naresh1.css
@@ -1,5 +1,14 @@
-!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="view
+document.getElementById("checkBtn").addEventListener("click", function () {
+    let num = document.getElementById("number").value;
+    let resultText;
+
+    if (num === "") {
+        resultText = "Please enter a number!";
+    } else if (num % 2 === 0) {
+        resultText = num + " is Even";
+    } else {
+        resultText = num + " is Odd";
+    }
+
+    document.getElementById("result").innerText = resultText;
+});
